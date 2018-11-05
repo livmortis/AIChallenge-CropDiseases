1a2202c31df2c6a43cacfd46630ba6fc24e1a17d
xzy <xzy@xzydeMacBook-Pro.local>
2018-11-05 16:54:37 +0800

    lock forward layers for reduce memory

--- acModel.py
+++ acModel.py
@@ -12,6 +12,8 @@ def main():
 def build_model():
     # alexnet = MODEL.alexnet(pretrained=True)
     resnet50 = MODEL.resnet50(pretrained=True)
+    for layer in resnet50.parameters():
+        layer.need_grad = False
     # alexnet.classifier = nn.Sequential(
     #     nn.Dropout(0.5),
     #     nn.Linear(9216, 4096),
